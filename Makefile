d77167797723b1aa5358ed489bf358d806ca1f3a
Volodymyr Vitvitskyi <volodymyr.vitvitskyi@vvitvitskyi-lt.vi.local>
2015-09-15 13:57:39 +0100

    Initial version

---
+++ Makefile
@@ -0,0 +1,6 @@
+turbot-new:
+	turbot bots:generate --bot demo --language python
+
+turbot-auth:
+	turbot auth:login
+
---
+++ scraper.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import json
+import datetime
+import turbotlib
+
+turbotlib.log("Starting run...") # Optional debug logging
+
+for n in range(0,20):
+    data = {"number": n,
+            "company": "Company %s Ltd" % n,
+            "message": "Hello %s" % n,
+            "sample_date": datetime.datetime.now().isoformat(),
+            "source_url": "http://somewhere.com/%s" % n}
+    # The Turbot specification simply requires us to output lines of JSON
+    print json.dumps(data)
